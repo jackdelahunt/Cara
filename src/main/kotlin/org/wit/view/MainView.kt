60e68ecde5562185fdc5a041d49f415e48272422
Jack Delahunt <jdelahun@redhat.com>
2021-10-03 16:10:57 +0100

    Feat: images are now displayed with tornadoFX

--- src/main/kotlin/org/wit/view/MainView.kt
+++ src/main/kotlin/org/wit/view/MainView.kt
@@ -1,12 +1,75 @@
 package org.wit.view
 
-import org.wit.Styles
+import javafx.geometry.Orientation
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.image.ImageView
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
+import org.wit.*
 import tornadofx.*
+import tornadofx.Stylesheet.Companion.button
+import tornadofx.Stylesheet.Companion.label
+import java.io.File
 
 class MainView : View("Hello TornadoFX") {
+
+    val controller: MainController by inject()
+
     override val root = hbox {
-        label(title) {
-            addClass(Styles.heading)
+        anchorpane {
+            prefWidth = 740.0
+            prefHeight = 540.0
+
+            vbox {
+                prefWidth = parent.prefWidth(-1.0)
+                prefHeight = parent.prefHeight(-1.0)
+
+                listmenu(theme = "blue") {
+                    prefWidth = parent.prefWidth(-1.0)
+                    orientation = Orientation.HORIZONTAL
+
+                    item(text = "All Images") {
+                        activeItem = this
+                    }
+                    item(text = "Groups")
+                    item(text = "Settings")
+                }
+
+                scrollpane {
+                    vgrow = Priority.ALWAYS
+
+                    flowpane {
+                        for (imageData in controller.filesystem.imageDataArray) {
+                            this += imageIcon(imageData)
+                        }
+                    }
+                }
+
+                listmenu(theme = "blue") {
+                    prefWidth = parent.prefWidth(-1.0)
+                    orientation = Orientation.HORIZONTAL
+
+                    item{}
+                }
+            }
         }
+    }
+}
+
+fun imageIcon(imageData: ImageData): HBox {
+    val hbox = HBox()
+    val button = Button()
+    button += ImageView(File("${imageDirectory}/${imageData.name}").toURI().toString())
+    hbox += button
+    hbox += Label(imageData.name)
+    return hbox
+}
+
+class MainController: Controller() {
+    var filesystem = FileSystem()
+
+    init {
+        filesystem.sync()
     }
 }
