03248f7a04e7cfd0478ad749268b5432b88025ab
jackdelahunt <jdelahun@redhat.com>
2021-10-13 11:43:46 +0100

    Fix: filesystem not saving on close

--- src/main/kotlin/org/wit/MyApp.kt
+++ src/main/kotlin/org/wit/MyApp.kt
@@ -4,4 +4,9 @@ import org.wit.styles.Styles
 import org.wit.view.allImagesView
 import tornadofx.App
 
-class MyApp: App(allImagesView::class, Styles::class)
+class MyApp: App(allImagesView::class, Styles::class) {
+    override fun stop() {
+        super.stop()
+        fileSystem().save()
+    }
+}
